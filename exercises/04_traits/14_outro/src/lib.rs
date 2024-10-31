3b2154b10c56bdf875fe1571879e30dbdfe287b5
Your Name <you@example.com>
2024-10-31 11:08:54 +0100

    14_outro

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -1,10 +1,97 @@
 // TODO: Define a new `SaturatingU16` type.
-//   It should hold a `u16` value.
-//   It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
+//   #It should hold a `u16` value.
+//   #It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
 //   It should support addition with a right-hand side of type
 //   SaturatingU16, u16, &u16, and &SaturatingU16. Addition should saturate at the
 //   maximum value for `u16`.
 //   It should be possible to compare it with another `SaturatingU16` or a `u16`.
-//   It should be possible to print its debug representation.
+//   #It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::{Add};
+use std::cmp::PartialEq;
+
+#[derive(Debug,Copy,Clone)]
+pub struct SaturatingU16
+{
+    value: u16,
+}
+
+impl PartialEq for SaturatingU16 {
+    fn eq(&self, other: &SaturatingU16) -> bool {
+        self.value == other.value
+    }
+}
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
+impl SaturatingU16
+{
+    pub fn new(value: u16) -> Self
+    {
+        SaturatingU16 { value: value }
+    }
+}
+impl Add for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.value.saturating_add( rhs.value))
+    }
+}
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        Self::new(self.value.saturating_add( rhs.value))
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: u16) -> Self::Output {
+        Self::new(self.value.saturating_add( rhs))
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u16) -> Self::Output {
+        Self::new(self.value.saturating_add( *rhs))
+    }
+}
+
+
+
+impl From<&u8> for SaturatingU16
+{
+    fn from(value: &u8) -> Self
+    {
+        SaturatingU16{value:*value as u16}
+    }
+}
+
+impl From<&u16> for SaturatingU16
+{
+    fn from(value: &u16) -> Self
+    {
+        SaturatingU16{value:*value}
+    }
+}
+
+impl Into<SaturatingU16> for u8{
+    fn into(self) -> SaturatingU16{
+        SaturatingU16{value:self as u16}
+    }
+}
+
+impl From<u16> for SaturatingU16
+{
+    fn from(value: u16) -> Self
+    {
+        SaturatingU16{value:value}
+    }
+}
