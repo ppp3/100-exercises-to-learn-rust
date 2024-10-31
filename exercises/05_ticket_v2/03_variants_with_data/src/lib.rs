33757361d681f294afd7e27017db832f7ae43891
Your Name <you@example.com>
2024-10-31 15:23:13 +0100

    03_variants_with_data

--- exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
+++ exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
@@ -37,8 +37,19 @@ impl Ticket {
             status,
         }
     }
+
+
     pub fn assigned_to(&self) -> &str {
-        todo!()
+        
+        match &self.status {
+            Status::InProgress { assigned_to } => {
+                assigned_to.as_str()
+            },
+            Status::ToDo | Status::Done => {
+                panic!("Only `In-Progress` tickets can be assigned to someone")
+            }
+        }
+
     }
 }
 
