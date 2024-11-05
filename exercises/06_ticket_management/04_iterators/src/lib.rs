27d4d7dbe5bf0039b4d251f0961ebe6f1d5f7081
Your Name <you@example.com>
2024-11-05 21:33:37 +0100

    04_iterators

--- exercises/06_ticket_management/04_iterators/src/lib.rs
+++ exercises/06_ticket_management/04_iterators/src/lib.rs
@@ -9,7 +9,14 @@ use ticket_fields::{TicketDescription, TicketTitle};
 pub struct TicketStore {
     tickets: Vec<Ticket>,
 }
+impl IntoIterator for TicketStore {
+    type Item = Ticket;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
 
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.into_iter()
+    }
+}
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ticket {
     pub title: TicketTitle,
